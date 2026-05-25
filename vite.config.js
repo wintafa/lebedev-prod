1e5290b23a3fc9ea0ca96098f7de06bf043cd8cf
wintafa <lebedewgre538@yandex.ru>
2026-05-25 21:36:04 +0300

    final fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // base: './' = относительные пути к ассетам.
 // Работает и на корне, и на /<repo>/ — не нужно знать имя репо заранее.
 export default defineConfig({
-  base: './',
+  base: '/lebedev-prod/',
   build: {
     outDir: 'dist',
   },
